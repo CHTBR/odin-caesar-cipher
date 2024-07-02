1e526143fed448aee2cf1ea5cbffd756512f35a2
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-02 21:38:31 +0200

    Modify order to only use positive values Removes the check for character bytecodes going below the range

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,19 +1,15 @@
 def caesar_cipher(message, order)
-  order = order.remainder(26)
+  order = order % 26
   encrypted_message = ""
   message.each_char do |char|
     if char.ord >= 97 && char.ord <= 122
-      if char.ord + order < 97
-        encrypted_message << (char.ord + order + 26).chr 
-      elsif char.ord + order > 122
+      if char.ord + order > 122
         encrypted_message << (char.ord + order - 26).chr
       else
         encrypted_message << (char.ord + order).chr
       end
     elsif char.ord >= 65 && char.ord <= 90
-      if char.ord + order < 65
-        encrypted_message << (char.ord + order + 26).chr 
-      elsif char.ord + order > 90
+      if char.ord + order > 90
         encrypted_message << (char.ord + order - 26).chr
       else
         encrypted_message << (char.ord + order).chr
