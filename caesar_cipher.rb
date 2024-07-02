d4404962c432bf3995fa793e82642d89c4537e16
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-02 21:19:13 +0200

    Add shifting of characters for lowercase letters Letters don't loop when going over a or z

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,12 @@
 def caesar_cipher(message, order)
   order = order.remainder(26)
+  encrypted_message = ""
+  message.each_char do |char|
+    if char.ord >= 97 && char.ord <= 122
+      encrypted_message << (char.ord + order).chr
+    else
+      encrypted_message << char
+    end
+  end
+  encrypted_message
 end
